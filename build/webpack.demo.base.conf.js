ee013e3f71286def881e602c30c6ced4fc35225f
youjie <youjie@hudongpai.com>
2018-11-28 20:26:47 +0800

    vue-loader解析

--- build/webpack.demo.base.conf.js
+++ build/webpack.demo.base.conf.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+//vue-loader15需要插件
+const { VueLoaderPlugin } = require("vue-loader");
 module.exports = {
   mode: "none",
   entry: { app: path.resolve(__dirname, "src/index.js") },
@@ -18,12 +19,17 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      {
+        test: /\.vue$/,
+        use: ["vue-loader"]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "example"
-    })
+    }),
+    new VueLoaderPlugin()
   ]
 };
