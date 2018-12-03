83f08c4e4e36adc8cf0af62e829c889a56e66279
youjie <youjie@hudongpai.com>
2018-12-03 16:36:30 +0800

    测试目录修改

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ds-button">
-    <button @click="handleClick">我是{{msg}}按钮</button>
+    <button>我是{{msg}}按钮</button>
     <span>{{msg+ds}}</span>
     <span class="msg">{{msg}}</span>
   </div>
@@ -8,21 +8,21 @@
 <script>
 export default {
   name: "ds-button",
-  data() {
-    return {
-      ds: "ds-ui"
-    };
-  },
+  // data() {
+  //   return {
+  //     ds: "ds-ui"
+  //   };
+  // },
   props: {
     msg: {
       require: true,
       type: String
     }
-  },
-  methods: {
-    handleClick() {
-      console.log(this.msg + this.ds);
-    }
   }
+  // methods: {
+  //   handleClick() {
+  //     console.log(this.msg + this.ds);
+  //   }
+  // }
 };
 </script>
