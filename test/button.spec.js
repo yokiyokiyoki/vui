eb645cb583c6ee0848a0a22341496aac5801992c
youjie <youjie@hudongpai.com>
2018-12-03 14:58:02 +0800

    babel-jest强制使用babel7.需要下载"babel-core": "^7.0.0-bridge.0"

---
+++ test/button.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import Message from "lib@/components/button/button.vue";
+
+describe("Button", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(Message, {
+      propsData: { msg }
+    });
+    expect(wrapper.text()).toBe(msg);
+  });
+});
