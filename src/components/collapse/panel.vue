2ac0e3da14190ec04eab38f38574df4560c47a50
youjie <youjie@datastory.com.cn>
2019-01-31 21:22:06 +0800

    feat(lib): 折叠栏组件
    增加panel

---
+++ src/components/collapse/panel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="panel-wrapper">2</div>
+</template>
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
