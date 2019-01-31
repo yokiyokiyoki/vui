81917f0219a2ab69e7b7aa683c2fa1aa75a177f6
youjie <youjie@datastory.com.cn>
2019-01-31 15:37:12 +0800

    refactor(lib): 组件加载方式
    使用数组循环加载

---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="ds-collapse-wrapper">1</div>
+</template>
+<script>
+export default {};
+</script>
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import Button from './components/button';
 import Input from './components/input';
+import Collapse from './components/collapse';
+
+const components = [Button, Input, Collapse];
+
 const install = function(Vue) {
   if (install.installed) return;
-  Vue.component(Button.name, Button);
-  Vue.component(Input.name, Input);
+  components.forEach(component => {
+    Vue.component(component.name, component);
+  });
 };
 
 // 有vue的话自动加载
@@ -14,5 +19,6 @@ if (typeof window !== 'undefined' && window.Vue) {
 export default {
   install,
   Button,
-  Input
+  Input,
+  Collapse
 };
