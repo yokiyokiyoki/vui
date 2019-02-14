038ee908289866d19864d6f027e6745b6d4f63a0
Chayin <chayin@datastory.com.cn>
2019-02-14 17:37:07 +0800

    feat(lib): 新增 icon 组件

--- src/components/icon/icon.vue
+++ src/components/icon/icon.vue
@@ -1,9 +1,61 @@
 <template>
-  <div class="ds-icon-wrapper">1</div>
+  <i :class="classes" :style="styles" @click="handleClick"></i>
 </template>
 <script>
+const prefixCls = 'ds-icon'
+import '../../styles/common/iconfont/iconfont.less'
+
 export default {
-  name: 'ds-icon'
+  name: 'ds-icon',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: [Number, String],
+      default: 14
+    },
+    spin: {
+      type: Boolean,
+      default: false
+    },
+    rotate: {
+      type: [Number, String],
+      default: 0
+    },
+    custom: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    classes() {
+      return [
+        `${prefixCls}`,
+        {
+          [`${prefixCls}-${this.type}`]: this.type !== '',
+          [`${this.custom}`]: this.custom !== '',
+          [`${prefixCls}-spin`]: this.spin,
+        }
+      ]
+    },
+    styles() {
+      return [
+        this.size && {
+          fontSize: `${this.size}px`
+        },
+        this.rotate && {
+          transform: `rotate(${this.rotate}deg)`
+        }
+      ]
+    }
+  },
+  methods: {
+    handleClick(e) {
+      this.$emit('click', e)
+    }
+  }
 };
 </script>
 
