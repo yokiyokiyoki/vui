53f0cd522ecbed2a33bf38c748b78bc28ce4866c
youjie <youjie@datastory.com.cn>
2019-02-15 11:50:39 +0800

    feat(内部demo): 引入样式
    使之动态变化

--- src/components/collapse/collapse.vue
+++ src/components/collapse/collapse.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="ds-collapse-wrapper">1
-    <panel/>
+  <div class="ds-collapse-wrapper">
+    <panel header="介绍">
+      <p>11</p>
+    </panel>
   </div>
 </template>
 <script>
@@ -9,6 +11,21 @@ export default {
   name: 'ds-collapse',
   components: {
     panel
+  },
+  props: {
+    activeKey: [Array, String],
+    defaultActiveKey: {
+      type: [Array, String],
+      required: true
+    },
+    accordion: {
+      type: Boolean,
+      default: false
+    },
+    destroyInactivePanel: {
+      type: Boolean,
+      default: false
+    }
   }
 };
 </script>
--- src/components/collapse/panel.vue
+++ src/components/collapse/panel.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="panel-wrapper">2</div>
+  <div class="ds-panel-wrapper">
+    <div class="ds-collapse-header">{{header}}</div>
+    <div class="ds-collapse-content">
+      <div class="ds-collapse-content-box">
+        <slot/>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
+  name: 'ds-panel',
   props: {
     disabled: {
       type: Boolean,
+      required: true
+    },
+    header: {
+      type: String,
+      required: true
+    },
+    key: {
+      type: String,
       required: true
     }
   }
