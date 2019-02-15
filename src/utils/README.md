283709da81a7166bc0e2d48942d551519b75d6cf
youjie <youjie@datastory.com.cn>
2019-02-16 01:22:29 +0800

    feat(lib): 工具类
    增加dom函数

--- src/utils/README.md
+++ src/utils/README.md
@@ -7,3 +7,9 @@
 - hasClass：探查是否有相应类名
 - addClass：添加相应类名
 - removeClass：删除相应类名
+
+### dom.js
+> 监听dom事件相关函数
+
+- on:添加监听事件
+- off：移除监听事件
---
+++ src/utils/dom.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+const isServer = Vue.prototype.$isServer;
+
+/**
+ * 说明：添加监听事件
+ * 用法： on(el,event,handler)
+ * 返回：void
+ * */
+const on = (function() {
+  if (!isServer && document.addEventListener) {
+    return function(element, event, handler) {
+      if (element && event && handler) {
+        element.addEventListener(event, handler, false);
+      }
+    };
+  } else {
+    return function(element, event, handler) {
+      if (element && event && handler) {
+        element.attachEvent('on' + event, handler);
+      }
+    };
+  }
+})();
+
+/**
+ * 说明：移除监听事件
+ * 用法： off(el,event,handler)
+ * 返回：void
+ * */
+const off = (function() {
+  if (!isServer && document.removeEventListener) {
+    return function(element, event, handler) {
+      if (element && event) {
+        element.removeEventListener(event, handler, false);
+      }
+    };
+  } else {
+    return function(element, event, handler) {
+      if (element && event) {
+        element.detachEvent('on' + event, handler);
+      }
+    };
+  }
+})();
+
+export default {
+  on,
+  off
+};
